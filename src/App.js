559b513fe9cad8fc8e713eb4196651daa53e0337
argie <argeline0729@yahoo.com>
2022-06-27 19:04:33 -0400

    api is working

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import axios from 'axios'
 
 function App() {
 
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+
   //got the api first from openweathermap.org
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=1647da47fb3f9d3c5aef9e617ce8ac18`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1647da47fb3f9d3c5aef9e617ce8ac18`
+
+const searchLocation = (event) => {
+  if (event.key === 'Enter') {
+  axios.get(url).then((response) => {
+    setData(response.data)
+    console.log(response.data)
+  })
+}
+}
+
 
   return (
     <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text"/>
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
@@ -22,13 +43,16 @@ function App() {
         </div>
         <div className="bottom">
           <div className="feels">
-            <p>65°F</p>
+            <p className='bold'>65°F</p>
+            <p>Feels Like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+            <p className='bold'>20%</p>
+            <p>Humidity</p>
           </div>
           <div className="wind">
-            12MPH
+            <p className='bold'>12 MPH</p>
+            <p>Wind Speed</p>
           </div>
         </div>
       </div>
